f9b2e0b51f1b3a9c809f5b35565fccac16b4af75
manger32 <manger32@gmail.com>
2023-09-02 21:52:45 +0300

    added SortingTypes

---
+++ dotnet/SelectionSort/Program.cs
@@ -0,0 +1,13 @@
+﻿using static Sorting;
+using static Infrastructure;
+
+// int[] array = CreateArray(10);
+// Show(array);
+// SortSelection(array);
+// Show(array);
+
+10.CreateArray(min: 10, max: 5)
+  .Show()
+  .SortSelection()
+  .Show();
+
